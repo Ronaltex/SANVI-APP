5bc28b88fee8d3ab3e874d0260f86d3541790328
Ronaltex <ronaltex@yori-tex.com>
2026-06-28 00:42:16 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,24 @@
-const CACHE_NAME = "sanvi-app-v3-20260623";
+const CACHE_NAME = "sanvi-app-v4-20260626";
 const APP_SHELL = [
   "./",
-  "./index.html?v=20260623-3",
-  "./manifest.webmanifest?v=20260623-3",
-  "./icon-192.png?v=20260623-3",
-  "./icon-512.png?v=20260623-3"
+  "./index.html?v=20260626-4",
+  "./manifest.webmanifest?v=20260626-4",
+  "./icon-192.png?v=20260626-4",
+  "./icon-512.png?v=20260626-4"
+];
+const EXTERNAL_SHELL = [
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE_NAME).then(async cache => {
+    await cache.addAll(APP_SHELL);
+    await Promise.allSettled(EXTERNAL_SHELL.map(async url => {
+      const response = await fetch(url);
+      if (response.ok) await cache.put(url, response);
+    }));
+  }).then(() => self.skipWaiting()));
 });
 
 self.addEventListener("activate", event => {
@@ -24,7 +34,8 @@ self.addEventListener("fetch", event => {
   const req = event.request;
   if (req.method !== "GET") return;
   const url = new URL(req.url);
-  if (url.origin !== self.location.origin) return;
+  const cacheableExternal = url.hostname === "unpkg.com" || url.hostname === "tile.openstreetmap.org";
+  if (url.origin !== self.location.origin && !cacheableExternal) return;
   event.respondWith(fetch(req).then(res => {
     const copy = res.clone();
     caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
